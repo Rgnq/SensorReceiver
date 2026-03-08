879187b8aba1daa6d4d42e8cd106721eb635db63
yuinen <1248237498@qq.com>
2026-03-08 19:04:32 +0800

    main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sys
 from PySide6.QtWidgets import QApplication
-
-
-from MainWindow import MainWindow
 from qt_material import apply_stylesheet
 
 
+from MainWindow import MainWindow
 
-if __name__ == "__main__":
+def main():
     app = QApplication(sys.argv)
     apply_stylesheet(app, theme='dark_teal.xml',invert_secondary=True)
     w = MainWindow()
     w.show()
     app.exec()
+
+
+if __name__ == "__main__":
+    main()
