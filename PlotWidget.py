bf092bd5a6695d9006ba3c9c8fb8c0a4b3ff91c6
yuinen <1248237498@qq.com>
2026-05-18 20:32:37 +0800

    重构传感器绘图组件，合并多个传感器图表为MultiPlotWidget，优化数据更新逻辑和UI布局

--- PlotWidget.py
+++ PlotWidget.py
@@ -1,6 +1,10 @@
-import time
-from PySide6.QtWidgets import QMainWindow, QTabWidget, QWidget, QVBoxLayout, QGridLayout, QFileDialog
+from PySide6.QtWidgets import (
+    QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QApplication, QScrollArea, QSizePolicy, QFileDialog
+)
+from PySide6.QtCore import QTimer, Qt
 import pyqtgraph as pg
+from datetime import datetime
+import sys
 from pyqtgraph.widgets.FileDialog import FileDialog
 
 original_file_dialog_init = FileDialog.__init__
@@ -14,201 +18,126 @@ def patched_init(self, *args, **kwargs):
     
 FileDialog.__init__ = patched_init
 
-class SensorPlotter(QMainWindow):
-    def __init__(self):
+class MultiPlotWidget(QWidget):
+    def __init__(self, plot_infos, max_points=100000):
+        """
+        plot_infos: [{"name": "name1", "unit": "unit1"}, ...]
+        max_points: 每条曲线最大点数
+        """
         super().__init__()
-        self.setWindowTitle("Sensor Data Plotter")
-        self.setGeometry(100, 100, 1200, 800)
+        self.plot_infos = plot_infos
+        self.max_points = max_points  # 最大点数
+        self.plots = {}  
+        self.curves = {}
+        self.data_x = {} # 保存时间戳
+        self.data_y = {}
 
-        self.start_time = time.time()
-        self.relative_time = True
+        self._setup_ui()
 
-        self.tab_widget = QTabWidget()
-        self.setCentralWidget(self.tab_widget)
+    def _setup_ui(self):
+        main_layout = QVBoxLayout(self)
+        main_layout.setContentsMargins(5,5,5,5)
+        main_layout.setSpacing(5)
 
-        # BH1750 Tab
-        bh_tab = QWidget()
-        bh_layout = QGridLayout()
-        self.bh_plots = {}
-        self.bh_curves = {}
-        self.bh_data = {name: {'x': [], 'y': []} for name in ['LIGHT']}
-        for i, name in enumerate(['LIGHT']):
-            pw = pg.PlotWidget()
-            pw.setTitle(name)
-            pw.setLabel('bottom', 'Time (s)')
-            pw.setLabel('left', '强度 (lux)')
-            curve = pw.plot(pen=pg.mkPen('r'))
-            self.bh_plots[name] = pw
-            self.bh_curves[name] = curve
-            bh_layout.addWidget(pw, i // 3, i % 3)  # 2 rows x 3 columns for better layout
-        bh_tab.setLayout(bh_layout)
-        self.tab_widget.addTab(bh_tab, "BH1750传感器")
+        # --- 顶部按钮 ---
+        top_layout = QHBoxLayout()
+        top_layout.addStretch()
+        self.btn_toggle_bg = QPushButton("背景切换")
+        self.btn_color_pick = QPushButton("曲线颜色")
+        top_layout.addWidget(self.btn_toggle_bg)
+        top_layout.addWidget(self.btn_color_pick)
+        main_layout.addLayout(top_layout)
 
-        # Gas Sensor Tab
-        gas_tab = QWidget()
-        gas_layout = QVBoxLayout()
-        self.gas_plots = {}
-        self.gas_curves = {}
-        self.gas_data = {name: {'x': [], 'y': []} for name in ['CO2', 'TVOC']}
-        for name in ['CO2', 'TVOC']:
-            pw = pg.PlotWidget()
-            pw.setTitle(name)
-            pw.setLabel('bottom', 'Time (s)')
-            pw.setLabel('left', 'Value')
-            curve = pw.plot(pen=pg.mkPen('g'))
-            self.gas_plots[name] = pw
-            self.gas_curves[name] = curve
-            gas_layout.addWidget(pw)
-        gas_tab.setLayout(gas_layout)
-        self.tab_widget.addTab(gas_tab, "气体传感器")
+        # --- 滚动绘图区域 ---
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_content = QWidget()
+        self.scroll_layout = QVBoxLayout(scroll_content)
+        self.scroll_layout.setContentsMargins(0,0,0,0)
+        self.scroll_layout.setSpacing(10)
 
-        # Temp Hum Press Sensor Tab
-        thp_tab = QWidget()
-        thp_layout = QVBoxLayout()
-        self.thp_plots = {}
-        self.thp_curves = {}
-        self.thp_data = {name: {'x': [], 'y': []} for name in ['温度', '湿度', '压强']}
-        for name in ['温度', '湿度', '压强']:
-            pw = pg.PlotWidget()
-            pw.setTitle(name)
-            pw.setLabel('bottom', 'Time (s)')
-            pw.setLabel('left', 'Value')
-            curve = pw.plot(pen=pg.mkPen('b'))
-            self.thp_plots[name] = pw
-            self.thp_curves[name] = curve
-            thp_layout.addWidget(pw)
-        thp_tab.setLayout(thp_layout)
-        self.tab_widget.addTab(thp_tab, "温湿压传感器")
+        scroll_area.setWidget(scroll_content)
+        main_layout.addWidget(scroll_area)
 
-        # Max data points to keep (to prevent memory issues)
-        self.max_points = 1000
+        # --- 创建图表 ---
+        for info in self.plot_infos:
+            pw = pg.PlotWidget(title=info["name"])
+            pw.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+            pw.setFixedHeight(200)
+            pw.showGrid(x=True, y=True)
+            pw.setLabel('left', text=info["unit"])
+            pw.setLabel('bottom', text="Time")
+            curve = pw.plot([], [], pen=pg.mkPen(width=2))
 
-    def _update_plot(self, data_dict, curve, x_list, y_list, value):
-        current_time = time.time() - self.start_time
-        if not self.relative_time:
-            self.use_relative_time_axis()
-        x_list.append(current_time)
-        y_list.append(value)
-        if len(x_list) > self.max_points:
-            x_list.pop(0)
-            y_list.pop(0)
-        curve.setData(x_list, y_list)
+            self.plots[info["name"]] = pw
+            self.curves[info["name"]] = curve
+            self.data_x[info["name"]] = []
+            self.data_y[info["name"]] = []
 
-    def update_bh(self, data):
+            self.scroll_layout.addWidget(pw)
+
+        self.scroll_layout.addStretch()
+
+    def update_data(self, data_list, display_count=40):
         """
-        Update BH1750 plots. data should be a dict with keys 'LIGHT'
+        data_list: [{'name': '温度', 'value': 25.3}, ...]
         """
-        for name in ['LIGHT']:
-            if name in data:
-                self._update_plot(self.bh_data[name], self.bh_curves[name],
-                                  self.bh_data[name]['x'], self.bh_data[name]['y'], data[name])
+        now_ts = datetime.now().timestamp()
+        for data in data_list:
+            name = data['name']
+            value = data['value']
+            if name in self.plots:
+                # 保存数据
+                self.data_x[name].append(now_ts)
+                self.data_y[name].append(value)
 
-    def update_gas(self, data):
-        """
-        Update Gas Sensor plots. data should be a dict with keys 'CO2', 'TVOC'
-        """
-        for name in ['CO2', 'TVOC']:
-            if name in data:
-                self._update_plot(self.gas_data[name], self.gas_curves[name],
-                                  self.gas_data[name]['x'], self.gas_data[name]['y'], data[name])
+                # 限制最大点数
+                if len(self.data_x[name]) > self.max_points:
+                    self.data_x[name] = self.data_x[name][-self.max_points:]
+                    self.data_y[name] = self.data_y[name][-self.max_points:]
 
-    def update_thp(self, data):
-        """
-        Update Temp Hum Press plots. data should be a dict with keys '温度', '湿度', '压强'
-        """
-        for name in ['温度', '湿度', '压强']:
-            if name in data:
-                self._update_plot(self.thp_data[name], self.thp_curves[name],
-                                  self.thp_data[name]['x'], self.thp_data[name]['y'], data[name])
+                # 更新曲线
+                self.curves[name].setData(self.data_x[name], self.data_y[name])
 
-    def reset(self):
-        """
-        Clear all plots, data, and reset time to zero.
-        """
-        self.start_time = time.time()
-        self.relative_time = True
-        data_groups = [self.bh_data, self.gas_data, self.thp_data]
-        curve_groups = [self.bh_curves, self.gas_curves, self.thp_curves]
-        for data_dict in data_groups:
-            for name in data_dict:
-                data_dict[name]['x'] = []
-                data_dict[name]['y'] = []
-        for curves in curve_groups:
-            for curve in curves.values():
-                curve.setData([], [])
+                # 横坐标显示格式化时间 & 间隔显示标签
+                tick_labels = []
+                interval = max(1, display_count // 8)
+                for i, ts in enumerate(self.data_x[name]):
+                    if i % interval == 0:
+                        tick_labels.append((ts, datetime.fromtimestamp(ts).strftime("%H:%M:%S")))
+                self.plots[name].getAxis('bottom').setTicks([tick_labels])
 
-    def load_bh_history(self, history_data):
-        """
-        Load historical data for BH1750 with absolute times.
-        history_data should be a dict like {'LIGHT': {'times': [t1, t2, ...], 'values': [v1, v2, ...]}, ...}
-        """
-        if self.relative_time:
-            self.relative_time = False
+                # 自动追随最新点，显示最近40个点
+                total_points = len(self.data_x[name])
+                if total_points <= display_count:
+                    x_min = self.data_x[name][0]
+                else:
+                    x_min = self.data_x[name][-display_count]
+                x_max = self.data_x[name][-1]
+                self.plots[name].setXRange(x_min, x_max, padding=0.05)
 
-            # 为所有 BH plot 设置日期时间轴（只需设置一次）
-            for name in self.bh_plots:
-                plot_widget = self.bh_plots[name]
-                # 替换默认的 AxisItem 为 DateAxisItem
-                plot_widget.setAxisItems({'bottom': pg.DateAxisItem(orientation='bottom')})
-                # 可选：调整显示格式（默认已经比较友好）
-                # plot_widget.getAxis('bottom').setLabel(text='时间')
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
 
-        for name in self.bh_data:
-            if name in history_data:
-                times = history_data[name]['times']
-                values = history_data[name]['values']
-                if len(times) == len(values):
-                    self.bh_data[name]['x'] = times
-                    self.bh_data[name]['y'] = values
-                    self.bh_curves[name].setData(times, values)
+    infos = [
+        {"name": "温度", "unit": "℃"},
+        {"name": "湿度", "unit": "%"},
+        {"name": "压力", "unit": "Pa"}
+    ]
+    widget = MultiPlotWidget(infos)
+    widget.resize(800, 600)
+    widget.show()
 
-    def load_gas_history(self, history_data):
-        """
-        Load historical data for Gas Sensor with absolute times.
-        history_data should be a dict like {'CO2': {'times': [t1, t2, ...], 'values': [v1, v2, ...]}, ...}
-        """
-        if self.relative_time:
-            self.relative_time = False
-            
-            # 为所有 Gas plot 设置日期时间轴（只需设置一次）
-            for name in self.gas_plots:
-                plot_widget = self.gas_plots[name]
-                plot_widget.setAxisItems({'bottom': pg.DateAxisItem(orientation='bottom')})
+    # 模拟动态更新
+    import random
+    def add_data():
+        widget.update_data([
+            {"name": "温度", "value": random.uniform(20,30)},
+            {"name": "湿度", "value": random.uniform(40,60)},
+            {"name": "压力", "value": random.uniform(1000,1100)}
+        ])
+    timer = QTimer()
+    timer.timeout.connect(add_data)
+    timer.start(100)
 
-        for name in self.gas_data:
-            if name in history_data:
-                times = history_data[name]['times']
-                values = history_data[name]['values']
-                if len(times) == len(values):
-                    self.gas_data[name]['x'] = times
-                    self.gas_data[name]['y'] = values
-                    self.gas_curves[name].setData(times, values)
-
-    def load_thp_history(self, history_data):
-        """
-        Load historical data for Temp Hum Press Sensor with absolute times.
-        history_data should be a dict like {'温度': {'times': [t1, t2, ...], 'values': [v1, v2, ...]}, ...}
-        """
-        if self.relative_time:
-            self.relative_time = False
-
-            # 为所有 THP plot 设置日期时间轴（只需设置一次）
-            for name in self.thp_plots:
-                plot_widget = self.thp_plots[name]
-                plot_widget.setAxisItems({'bottom': pg.DateAxisItem(orientation='bottom')})
-
-        for name in self.thp_data:
-            if name in history_data:
-                times = history_data[name]['times']
-                values = history_data[name]['values']
-                if len(times) == len(values):
-                    self.thp_data[name]['x'] = times
-                    self.thp_data[name]['y'] = values
-                    self.thp_curves[name].setData(times, values)
-
-    def use_relative_time_axis(self):
-        self.relative_time = True
-        for plots in [self.bh_plots, self.gas_plots, self.thp_plots]:
-            for pw in plots.values():
-                pw.setAxisItems({'bottom': pg.AxisItem(orientation='bottom')})
-                pw.setLabel('bottom', 'Time (s)')
+    sys.exit(app.exec())
